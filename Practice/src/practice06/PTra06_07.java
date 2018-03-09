bcd1056047e93a173fa3d471df15fc19b07ddae4
DESKTOP-0MMHI1A\mound <liverpoolvseverton@gmail.com>
2018-03-09 18:00:50 +0900

    8-1

--- Practice/src/practice06/PTra06_07.java
+++ Practice/src/practice06/PTra06_07.java
@@ -25,5 +25,52 @@ public class PTra06_07 {
 		 * ■□□□□
 		 */
 
-	}
-}
+
+
+		for(int i =0;i<5; i++) {
+			for(int j =0;j<i; j++) {
+				System.out.print("■");
+			}
+
+			for(int j =5;j>i; j--) {
+				System.out.print("□");
+			}
+
+
+			System.out.println();
+
+
+
+		}
+
+
+
+		for(int i =0;i<5; i++) {
+			for(int j =0;j+i<=4; j++) {
+				System.out.print("■");
+			}
+
+			for(int j =5;j+i>=6; j--) {
+				System.out.print("□");
+			}
+
+
+			System.out.println();
+
+
+
+		}
+
+
+
+
+
+			}
+
+
+
+
+
+		}
+
+
